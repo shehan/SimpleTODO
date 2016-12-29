00640f7a5fe408aa2abebcb4b81a3004cd018cb4
shehan peruma <shehan.peruma@gmail.com>
2016-12-29 17:27:46 -0500

    added action bar menu items to the MyTODO activity

--- app/src/main/java/lk/peruma/simpletodo/MyTODOsActivity.java
+++ app/src/main/java/lk/peruma/simpletodo/MyTODOsActivity.java
@@ -7,6 +7,9 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListAdapter;
@@ -24,6 +27,7 @@ public class MyTODOsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_todos);
+
 
         mainListView = (ListView) findViewById(R.id.myTODOListView);
         actionButton = (FloatingActionButton) findViewById(R.id.floatingActionButton);
@@ -56,6 +60,28 @@ public class MyTODOsActivity extends AppCompatActivity {
     private void loadListView(){
         adapter  = new CustomAdapter(this, SimpleTODO.GetAllTODOs(this));
         mainListView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_about) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
