44283f1c6905100736b17bdf4f5a04fe056ca8df
shehan peruma <shehan.peruma@gmail.com>
2016-12-29 12:14:20 -0500

    change the display format of the due date

--- app/src/main/java/lk/peruma/simpletodo/CustomAdapter.java
+++ app/src/main/java/lk/peruma/simpletodo/CustomAdapter.java
@@ -9,15 +9,21 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.Locale;
 
 import static android.R.attr.resource;
 
 
 public class CustomAdapter extends ArrayAdapter<SimpleTODO> {
 
+    private SimpleDateFormat dateFormatter;
+
     public CustomAdapter(Context context, List<SimpleTODO> TODOs) {
         super(context,R.layout.custom_row, TODOs);
+
+        dateFormatter = new SimpleDateFormat("dd-MMM-yyyy", Locale.US);
     }
 
     @NonNull
@@ -26,12 +32,12 @@ public class CustomAdapter extends ArrayAdapter<SimpleTODO> {
         LayoutInflater inflater = LayoutInflater.from(getContext());
         View customView = inflater.inflate(R.layout.custom_row,parent,false);
 
-        SimpleTODO singleFoodItem = getItem(position);
+        SimpleTODO singleItem = getItem(position);
         TextView itemTitle = (TextView) customView.findViewById(R.id.textViewTitle);
         TextView itemDue = (TextView) customView.findViewById(R.id.textViewDue);
 
-        itemTitle.setText(singleFoodItem.getTitle());
-        itemDue.setText(singleFoodItem.getDue().toString());
+        itemTitle.setText(singleItem.getTitle());
+        itemDue.setText("Due: " + dateFormatter.format(singleItem.getDue()));
 
 
         return customView;
