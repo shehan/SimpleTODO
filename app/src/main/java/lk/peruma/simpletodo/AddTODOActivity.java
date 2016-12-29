a99e4bcfe3d3aa85c192d6b7208d3b08acce5866
shehan peruma <shehan.peruma@gmail.com>
2016-12-29 01:18:47 -0500

    added functionality to insert TODO

--- app/src/main/java/lk/peruma/simpletodo/AddTODOActivity.java
+++ app/src/main/java/lk/peruma/simpletodo/AddTODOActivity.java
@@ -4,12 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class AddTODOActivity extends AppCompatActivity {
 
     Button buttonSave, buttonCancel;
-    EditText textTitle, textDescription, textDue;
+    EditText textTitle, textDescription;
+    DatePicker dateDue;
 
 
     @Override
@@ -22,7 +28,8 @@ public class AddTODOActivity extends AppCompatActivity {
 
         textTitle = (EditText) findViewById(R.id.editTextTitle);
         textDescription = (EditText) findViewById(R.id.editTextDescription);
-       // textDue = (EditText) findViewById(R.id.editTextDueDate);
+
+        dateDue = (DatePicker) findViewById(R.id.datePickerDueDate);
 
         buttonCancel.setOnClickListener(
                 new View.OnClickListener() {
@@ -37,12 +44,32 @@ public class AddTODOActivity extends AppCompatActivity {
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-
+                        String title = textTitle.getText().toString();
+                        String description = textDescription.getText().toString();
+                        Date dueDate  = getDateFromDatePicket(dateDue);
+                        SimpleTODO simpleTODO = new SimpleTODO(AddTODOActivity.this,title,description,dueDate);
+                        if (simpleTODO.Save()){
+                            Toast.makeText(AddTODOActivity.this, "TODO Successfully Saved!", Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            Toast.makeText(AddTODOActivity.this, "TODO Not Saved", Toast.LENGTH_SHORT).show();
+                        }
                     }
                 }
         );
 
 
 
+    }
+
+    private static java.util.Date getDateFromDatePicket(DatePicker datePicker){
+        int day = datePicker.getDayOfMonth();
+        int month = datePicker.getMonth();
+        int year =  datePicker.getYear();
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month, day);
+
+        return calendar.getTime();
     }
 }
