9c8372bec8bf1252e42b3412371db10b379810c2
shehan peruma <shehan.peruma@gmail.com>
2016-12-29 11:54:09 -0500

    replaced the calendar control with the date dialog control. enable the save button, description textbox and date textbox when the title has content.

--- app/src/main/java/lk/peruma/simpletodo/AddTODOActivity.java
+++ app/src/main/java/lk/peruma/simpletodo/AddTODOActivity.java
@@ -1,21 +1,29 @@
 package lk.peruma.simpletodo;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 public class AddTODOActivity extends AppCompatActivity {
 
     Button buttonSave, buttonCancel;
-    EditText textTitle, textDescription;
-    DatePicker dateDue;
+    EditText textTitle, textDescription, textDue;
+    DatePickerDialog dateDue;
+    Date dueDate;
+    private SimpleDateFormat dateFormatter;
 
 
     @Override
@@ -23,13 +31,60 @@ public class AddTODOActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_todo);
 
+        dateFormatter = new SimpleDateFormat("dd-MM-yyyy", Locale.US);
+
         buttonCancel = (Button) findViewById(R.id.buttonCancel);
         buttonSave = (Button) findViewById(R.id.buttonSave);
 
         textTitle = (EditText) findViewById(R.id.editTextTitle);
         textDescription = (EditText) findViewById(R.id.editTextDescription);
+        textDue = (EditText) findViewById(R.id.datePickerDueDate);
 
-        dateDue = (DatePicker) findViewById(R.id.datePickerDueDate);
+        textTitle.addTextChangedListener(new TextWatcher() {
+                                             @Override
+                                             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                                             }
+
+                                             @Override
+                                             public void onTextChanged(CharSequence s, int start, int before, int count) {
+                                                 if(textTitle.getText().length()>0){
+                                                     buttonSave.setEnabled(true);
+                                                     textDue.setEnabled(true);
+                                                     textDescription.setEnabled(true);
+                                                 }
+                                                 else{
+                                                     buttonSave.setEnabled(false);
+                                                     textDue.setEnabled(false);
+                                                     textDescription.setEnabled(false);
+                                                 }
+                                             }
+
+                                             @Override
+                                             public void afterTextChanged(Editable s) {
+                                             }
+                                         }
+        );
+
+        textDue.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dateDue.show();
+                    }
+                }
+        );
+
+        Calendar newCalendar = Calendar.getInstance();
+        dateDue = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, month, dayOfMonth);
+                textDue.setText(dateFormatter.format(newDate.getTime()));
+                dueDate = newDate.getTime();
+            }
+        }, newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+
 
         buttonCancel.setOnClickListener(
                 new View.OnClickListener() {
@@ -46,12 +101,11 @@ public class AddTODOActivity extends AppCompatActivity {
                     public void onClick(View v) {
                         String title = textTitle.getText().toString();
                         String description = textDescription.getText().toString();
-                        Date dueDate  = getDateFromDatePicket(dateDue);
-                        SimpleTODO simpleTODO = new SimpleTODO(AddTODOActivity.this,title,description,dueDate);
-                        if (simpleTODO.Save()){
+                        Date due = dueDate; //getDateFromDatePicket(dateDue);
+                        SimpleTODO simpleTODO = new SimpleTODO(AddTODOActivity.this, title, description, due);
+                        if (simpleTODO.Save()) {
                             Toast.makeText(AddTODOActivity.this, "TODO Successfully Saved!", Toast.LENGTH_SHORT).show();
-                        }
-                        else{
+                        } else {
                             Toast.makeText(AddTODOActivity.this, "TODO Not Saved", Toast.LENGTH_SHORT).show();
                         }
                     }
@@ -59,13 +113,12 @@ public class AddTODOActivity extends AppCompatActivity {
         );
 
 
-
     }
 
-    private static java.util.Date getDateFromDatePicket(DatePicker datePicker){
+    private static java.util.Date getDateFromDatePicket(DatePicker datePicker) {
         int day = datePicker.getDayOfMonth();
         int month = datePicker.getMonth();
-        int year =  datePicker.getYear();
+        int year = datePicker.getYear();
 
         Calendar calendar = Calendar.getInstance();
         calendar.set(year, month, day);
